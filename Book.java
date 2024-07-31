9a8d6064607635e34352206ba5faf4b9f10946da
Ridma-kithara <ridmakithara19@gmail.com>
2024-07-31 15:01:44 +0530

    Update Book.java

--- Book.java
+++ Book.java
@@ -4,13 +4,15 @@ public class Book {
     private String id;
     private String title;
     private String author;
-    private boolean isBorrowed;
+    private String subject;
+    private boolean borrowed;
 
-    public Book(String id, String title, String author) {
+    public Book(String id, String title, String author, String subject) {
         this.id = id;
         this.title = title;
         this.author = author;
-        this.isBorrowed = false;
+        this.subject = subject;
+        this.borrowed = false;
     }
 
     public String getId() {
@@ -25,21 +27,25 @@ public class Book {
         return author;
     }
 
-    public boolean isBorrowed() {
-        return isBorrowed;
+    public String getSubject() {
+        return subject;
     }
 
-    public void borrow() {
-        if (!isBorrowed) {
-            isBorrowed = true;
+    public boolean isBorrowed() {
+        return borrowed;
+    }
+
+    public void borrowBook() {
+        if (!borrowed) {
+            borrowed = true;
         } else {
             System.out.println("Book is already borrowed.");
         }
     }
 
     public void returnBook() {
-        if (isBorrowed) {
-            isBorrowed = false;
+        if (borrowed) {
+            borrowed = false;
         } else {
             System.out.println("Book was not borrowed.");
         }
